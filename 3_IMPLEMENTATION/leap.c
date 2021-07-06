fda35669e4e8d9bb2b64afc81909a1d8a4e84e34
SAIRAJ L PATGAR <69588986+SAIRAJL@users.noreply.github.com>
2021-07-06 15:20:25 +0530

    Update leap.c

--- 3_IMPLEMENTATION/leap.c
+++ 3_IMPLEMENTATION/leap.c
@@ -1,1 +1,19 @@
-
+#include "head.h"
+int leapyear(int x){
+    if(x%4==0){
+        if(x%100==0){
+            if(x%400==0){
+                return 1;
+            }
+            else{
+                return 0;
+            }
+        }
+        else{
+            return 1;
+        }
+    }
+    else{
+        return 0;
+    }
+}
