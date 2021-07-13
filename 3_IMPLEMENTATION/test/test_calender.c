71fe0822ba1c70b1237b76fa2e9bc8f41c388743
SAIRAJ L PATGAR <69588986+SAIRAJL@users.noreply.github.com>
2021-07-13 19:28:15 +0530

    Update test_calender.c

--- 3_IMPLEMENTATION/test/test_calender.c
+++ 3_IMPLEMENTATION/test/test_calender.c
@@ -9,7 +9,7 @@
  * 
  */
 #include "unity.h"
-#include "Header_for_TIC.h"
+#include "Head.h"
 void setUp()
 {
 }
@@ -17,24 +17,24 @@ void tearDown()
 {
 }
 
-/*void test_check_horiziontal(void)
+void test_check_leapyear(void)
 {
-    TEST_ASSERT_EQUAL(1,Win_Lose_CHECK(box[1]==box[2] && box[2]==box[3]));
-    TEST_ASSERT_EQUAL(1,Win_Lose_CHECK(box[4]==box[5] && box[5]==box[6]));
-    TEST_ASSERT_EQUAL(1,Win_Lose_CHECK(box[7]==box[8] && box[8]==box[9]));
+    TEST_ASSERT_EQUAL(1,leapyear(2011));
+    TEST_ASSERT_EQUAL(1,leapyear(1900));
+    TEST_ASSERT_EQUAL(1,leapyear(2000));
+  
+    
 }
-void test_check_vertical(void)
+void test_check_leapyear(void)
 {
- 
-  TEST_ASSERT_EQUAL(1,Win_Lose_CHECK(box[1]==box[4] && box[4]==box[7]));
-  TEST_ASSERT_EQUAL(1,Win_Lose_CHECK(box[2]==box[5] && box[5]==box[8]));
-  TEST_ASSERT_EQUAL(1,Win_Lose_CHECK(box[3]==box[6] && box[6]==box[9]));
+    TEST_ASSERT_EQUAL(1,calender(5));
+    TEST_ASSERT_EQUAL(1,calender(2));
+
+  
+    
 }
-void test_check_diagonal(void)
-{
-  TEST_ASSERT_EQUAL(1,Win_Lose_CHECK(box[1]==box[5] && box[5]==box[9]));
-  TEST_ASSERT_EQUAL(1,Win_Lose_CHECK(box[3]==box[5] && box[5]==box[7]));
-}*/
+
+
 
 int main(void)
 {
